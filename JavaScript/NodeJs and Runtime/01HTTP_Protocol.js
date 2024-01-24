cc76ed81e27c418bcd3fe94c3adc1fdb1b4314fd
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-23 20:53:32 -0600

    Added the code

---
+++ JavaScript/NodeJs and Runtime/01HTTP_Protocol.js
@@ -0,0 +1,33 @@
+// 1. Protocol :  is defined for machine to communicate
+// 2. Specifically for websited, it is the most common way for your website front-end to talk to backend.
+
+// Front-End : UserInterface, the UI we see with our eyes. (HTML/CSS/Node.js)
+// Back-End : A place where our code runs and generate the response for our question (Which we can't see how our question is 
+//going to server and talk to the API's and coming back to us with the anser) (Node.js)
+
+/*
+HTTP (Hyper Text Transfer Protocol):
+
+You should know below things before creating the HTTP :
+
+1. Protocol (HTTP/HTTPS)
+2. Address (URL/IP/PORT)
+3. Route 
+4. HeaderBody Query Params 
+5. Method 
+
+IP : Internet Protocol IPv4 and IPv6 
+DNS : Domain Name Service resolution  : People buy domain and register that with the IP 
+
+Common methods you can send to your BE (Back End Server) ?
+1. GET 
+2. POST 
+3. PUT
+4. DELETE
+
+Status Codes :
+200 : Everything is ok
+404 : Page/route not found
+403 : Authentication Issues
+500 : Internal Server issues 
+*/
